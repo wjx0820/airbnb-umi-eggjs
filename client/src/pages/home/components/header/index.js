813d0fa05bfe98ce9e6d8dd9a282676c9e83a81a
Jexxie <1989wjx@gmail.com>
2020-11-07 19:17:06 +0800

    feat:前端首页

---
+++ client/src/pages/home/components/header/index.js
@@ -0,0 +1,23 @@
+import React, { useState, useEffect, memo } from 'react';
+import { Link } from 'umi';
+// import { cookie } from 'project-libs';
+
+function Header(props) {
+  const [state, setState] = useState();
+  console.log('header render');
+
+  useEffect(() => {
+    // console.log(cookie.get('user'))
+  }, []);
+
+  return (
+    <div className="header">
+      <div className="header_title">民宿</div>
+      <div className="header_login">
+        <Link to="/login"> 登录</Link> | <Link to="/register"> 注册</Link>
+      </div>
+    </div>
+  );
+}
+
+export default memo(Header);
---
+++ client/src/pages/home/components/hot/index.js
@@ -0,0 +1,73 @@
+import React, { useState, useEffect, memo } from 'react';
+import { history } from 'umi';
+
+function Hot(props) {
+  const [houses, setHouses] = useState([
+    {
+      id: 1,
+      img:
+        'http://img3.mukewang.com/szimg/5d1032ab08719e0906000338-360-202.jpg',
+      title: '东城民宿',
+      info: '东城区交通方便',
+      price: '100',
+    },
+    {
+      id: 2,
+      img:
+        'http://img2.mukewang.com/szimg/5dc9047a09bae31e12000676-360-202.png',
+      title: '西城民宿',
+      info: '西城区山水怡情',
+      price: '120',
+    },
+    {
+      id: 3,
+      img:
+        'http://img2.mukewang.com/szimg/5ad05dc00001eae705400300-360-202.jpg',
+      title: '新区民宿',
+      info: '新区民宿位置优越',
+      price: '200',
+    },
+    {
+      id: 4,
+      img:
+        'http://img1.mukewang.com/szimg/5a1f65a900015d1505400300-360-202.jpg',
+      title: '老城民宿',
+      info: '老城区风景秀美',
+      price: '220',
+    },
+  ]);
+
+  // console.log('hot render')
+  // const handleClick = (id) => {
+  //   history.push({
+  //     pathname: '/house',
+  //     query: {
+  //       id
+  //     }
+  //   });
+  // }
+
+  useEffect(() => {}, []);
+
+  return (
+    <div className="hot">
+      <h1>最热民宿</h1>
+      <div className="hot-lists">
+        {houses.map(item => (
+          <div
+            className="hot-lists-item"
+            key={item.id}
+            // onClick={() => handleClick(item.id)}
+          >
+            <img className="img" alt="img" src={item.img} />
+            <div className="title">{item.title}</div>
+            <div className="info">{item.info}</div>
+            <div className="price">￥{item.price}</div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default memo(Hot);
