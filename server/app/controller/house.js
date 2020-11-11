875ff86890937f3fd60146b1158e00c652ad23e1
Jexxie <1989wjx@gmail.com>
2020-11-11 12:17:23 +0800

    feat: 搜索民宿接口

--- server/app/controller/house.js
+++ server/app/controller/house.js
@@ -6,6 +6,12 @@ class HouseController extends BaseController {
     const result = await ctx.service.house.hot();
     this.success(result);
   }
+
+  async search() {
+    const { ctx, app } = this;
+    const result = await ctx.service.house.search(ctx.params());
+    this.success(result);
+  }
 }
 
 module.exports = HouseController;
